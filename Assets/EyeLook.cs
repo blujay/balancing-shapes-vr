d137cce469105d71516c778ed37bd0bc3bc3e216
Maf'j Alvarez <jay@rootinteractive.com>
2019-05-30 13:11:27 +0100

    Added eyes that follow you

---
+++ Assets/EyeLook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLook : MonoBehaviour
+{
+    public Transform eyeDest;
+  
+    void Update()
+    {
+        transform.LookAt(eyeDest);
+    }
+}
